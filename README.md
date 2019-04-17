8765d0634532c32a16fa510e2da2c0f14a52e754
Tom Donohue <monodot@gmail.com>
2019-04-17 16:19:16 +0100

    Add Maven settings

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # workstation
+
+Stuff for my MacBook or Fedora.
+
+## Maven
+
+Included in `maven/` are example `settings.xml` files to use.
+
+
---
+++ maven/configure-maven.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f ~/.m2/settings.xml ]; then
+	mv ~/.m2/settings.xml ~/.m2/settings.xml.bak
+fi
+
+ln -s $(pwd)/settings-mirror.xml ~/.m2/settings.xml
+ln -s $(pwd)/settings-redhat.xml ~/.m2/settings-redhat.xml
+
