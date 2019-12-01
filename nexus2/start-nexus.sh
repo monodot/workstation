bb1980488d7d4a8fe130c64e892e9f3adedbf9a4
Tom Donohue <monodot@gmail.com>
2019-12-01 13:43:17 +0000

    Change Nexus to use podman

--- nexus2/start-nexus.sh
+++ nexus2/start-nexus.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-docker run --rm -d -p 8085:8081 --name nexus -v /opt/nexus/nexus-data:/sonatype-work:z sonatype/nexus
+
+NEXUS_VOLUME=$HOME/.local/share/nexus2
+NEXUS_PORT=8085
+
+if [ ! -d $NEXUS_VOLUME ]; then
+        mkdir -p $NEXUS_VOLUME/nexus-data/sonatype-work/conf
+        echo "Created Nexus volume directory"
+	cp "$(dirname "${BASH_SOURCE}")/nexus.xml" $NEXUS_VOLUME/nexus-data/sonatype-work/conf/nexus.xml
+        echo "Copied default nexus.xml to conf directory"
+fi
+
+podman run --rm --name nexus2 -p $NEXUS_PORT:8081 -v $NEXUS_VOLUME:/sonatype-work:Z --userns=keep-id -d sonatype/nexus
+
+echo "Nexus running shortly on http://localhost:$NEXUS_PORT"
 
